a8b23525f1c37532c49a450ba884485908cd8df2
amandasanto05 <129517113+amandasanto05@users.noreply.github.com>
2024-05-23 12:21:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaprincipal = document.querySelector(".caixa-principal");
+const caixaperguntas = document.querySelector(".caixa-perguntas");
+const caixaalternativas = document.querySelector(".caixa-alternativas");
+const caixaresultado = document.querySelector(".caixa-resultado");
+const textoresultado = document.querySelector(".texto-resultado");
+{
+    const perguntas = [
+        enunciado: "pergunta 1", 
+        alternativas: 
+            "Alternativa 1", 
+            "Alternativa 2"]
+}
+
+    "pergunta 2"
+
+    {
+        const perguntas = [
+            enunciado: "pergunta 1", 
+            alternativas: 
+                "Alternativa 1", 
+                "Alternativa 2"]
+    
